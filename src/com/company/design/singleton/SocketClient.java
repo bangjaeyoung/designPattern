ef68dfc16881711193c9c5cd3e4a1b1d18bc51e8
bangjaeyoung <qkdwo4613@gmail.com>
2022-07-13 14:36:37 +0900

    first commit

--- src/com/company/design/singleton/SocketClient.java
+++ src/com/company/design/singleton/SocketClient.java
@@ -4,7 +4,9 @@ public class SocketClient {
 
     private static SocketClient socketClient = null;
 
-    private SocketClient() {}
+    private SocketClient() {
+
+    }
 
     public static SocketClient getInstance() {
         if(socketClient == null) {
