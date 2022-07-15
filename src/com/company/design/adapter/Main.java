44b43c47bf226db407ebb3dc9053fa2f7e9d3489
bangjaeyoung <qkdwo4613@gmail.com>
2022-07-15 13:43:35 +0900

    first commit

--- src/com/company/design/adapter/Main.java
+++ src/com/company/design/adapter/Main.java
@@ -1,4 +1,21 @@
 package com.company.design.adapter;
 
 public class Main {
+    public static void main(String[] args) {
+
+        HairDryer hairDryer = new HairDryer();
+        conenct(hairDryer);
+
+        Cleaner cleaner = new Cleaner();
+        Electronic110V adapter = new SocketAdapter(cleaner);
+        conenct(adapter);
+
+        AirConditioner airConditioner = new AirConditioner();
+        Electronic110V airAdapter = new SocketAdapter(airConditioner);
+        conenct(airAdapter);
+    }
+
+    public static void conenct(Electronic110V electronic110V) {
+        electronic110V.powerOn();
+    }
 }
--- src/com/company/design/proxy/Main.java
+++ src/com/company/design/proxy/Main.java
@@ -1,4 +1,50 @@
 package com.company.design.proxy;
 
+import com.company.design.aop.AopBrowser;
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+//        Browser browser = new Browser("www.naver.com");
+//        browser.show();
+//        browser.show();
+//        browser.show();
+//        browser.show();
+//        browser.show();
+//        // 매번 로딩이 일어남
+
+//        IBrowser browser = new BrowserProxy("www.naver.com");
+//        browser.show();
+//        browser.show();
+//        browser.show();
+//        browser.show();
+//        browser.show();
+//        // 처음만 로딩을 하고 그 다음부터는 cache된 결과만 나옴
+
+
+        //AopBrowser
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
+
+        IBrowser aopBrowser = new AopBrowser("www.naver.com",
+                ()->{
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+                },
+                ()-> {
+                    long now = System.currentTimeMillis();
+                    end.set(now - start.get());
+                }
+                );
+
+        aopBrowser.show();
+        System.out.println("loading time : " + end.get());
+        // 첫 번째 로딩 타임은 1.5초
+
+        aopBrowser.show();
+        System.out.println("loading time : " + end.get());
+        // 두 번째 로딩 타임은 0초 (캐시를 이용했기 때문)
+    }
 }
